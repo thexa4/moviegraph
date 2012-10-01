bc7ba3c7fd4a51be2c776670fb747f5dbaf8a877
thexa4 <thexa4@gmail.com>
2012-10-01 21:21:27 +0200

    Added data model

---
+++ MovieData/Data/Actor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MovieGraph.Data
+{
+    class Actor
+    {
+        string Name { get; set; }
+        List<Movie> Movies { get; protected set; }
+    }
+}
---
+++ MovieData/Data/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MovieGraph.Data
+{
+    class Movie
+    {
+        string IMDBId { get; set; }
+        string Name { get; set; }
+        int Year { get; set; }
+        List<Actor> Actors { get; protected set; }
+        List<string> Genres { get; protected set; }
+        string Poster { get; set; }
+        float Rating { get; set; }
+        string Plot { get; set; }
+    }
+}
