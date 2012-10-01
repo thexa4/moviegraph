80c19c52da6d747d9a26f594b2a5da4d19474d71
thexa4 <thexa4@gmail.com>
2012-10-01 21:37:39 +0200

    Added JSON parser

---
+++ MovieData/Service/Imdb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace MovieData.Service
+{
+    class Imdb
+    {
+        const string Endpoint = "http://www.deanclatworthy.com/imdb/";
+
+        static void 
+    }
+}
